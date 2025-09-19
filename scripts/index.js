afeb057449e85dd18893c8593875e14d16cba1a1
olaniyimercy <olaniyimercy45@gmail.com>
2025-09-19 09:30:18 +0100

    done

--- scripts/index.js
+++ scripts/index.js
@@ -11,8 +11,7 @@ fetch('https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2Cconten
       videoList.setAttribute("class", "video-list");
       videoList.innerHTML = `
         <h3>${title}</h3>
-        <iframe src="https://www.youtube.com/embed/videoseries?list=${myVideo}" frameborder="0" allowfullscreen></iframe>
-      `;
+        <iframe src="https://www.youtube.com/embed/videoseries?list=${myVideo}" frameborder="0" allowfullscreen></iframe>`;
       container.appendChild(videoList);
     });
   })
